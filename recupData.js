12a0c4ac65b1f21fbe3d2c8abbd4045207aad7a0
Maxou <105202869+FLKprod@users.noreply.github.com>
2024-02-05 19:54:45 -0500

    Mise a jour du tableau et ajout de fonction refresh fonctionnelle

--- recupData.js
+++ recupData.js
@@ -1,7 +1,8 @@
 console.log("script.js loaded");
+loadTableData();
 
 document.addEventListener("DOMContentLoaded", function() {
-    loadTableData();
+    
 
     document.getElementById('refreshButton').addEventListener('click', function() {
         loadTableData();
@@ -11,10 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function loadTableData() {
     var table = document.getElementById('vulnerabilityTable');
 
-    // Clear existing table
-    while (table.firstChild) {
-        table.removeChild(table.firstChild);
-    }
+    
 
     // Ajouter un timestamp pour forcer le rafraîchissement du fichier JSON
     var timestamp = new Date().getTime();
@@ -23,6 +21,11 @@ function loadTableData() {
     fetch(url)
         .then(response => response.json())
         .then(data => {
+
+            // Clear existing table
+            while (table.firstChild) {
+                table.removeChild(table.firstChild);
+            }
             // Vérifier si data est un tableau et s'il contient au moins une ligne
             if (Array.isArray(data) && data.length > 0) {
                 // 1ere ligne = en-têtes
@@ -31,13 +34,19 @@ function loadTableData() {
                     var cell = headerRow.insertCell();
                     cell.appendChild(document.createTextNode(header));
                 });
+                headerRow.classList.add('header-row');
 
                 // Ajouter des lignes de data.json (à partir de la deuxième ligne)
                 data.forEach(rowData => {
                     var row = table.insertRow();
-                    Object.values(rowData).forEach(cellData => {
+                    Object.entries(rowData).forEach(([key, value]) => {
                         var cell = row.insertCell();
-                        cell.appendChild(document.createTextNode(cellData));
+                        cell.appendChild(document.createTextNode(value));
+
+                        // Ajouter une classe en fonction de la dernière colonne
+                        if (key === Object.keys(rowData).pop()) {
+                            cell.className = evaluateRisk(value);
+                        }
                     });
                 });
             } else {
@@ -46,3 +55,18 @@ function loadTableData() {
         })
         .catch(error => console.error('Erreur de chargement des données JSON:', error));
 }
+
+function evaluateRisk(value) {
+    // Ajouter des conditions pour déterminer la classe en fonction de la valeur
+    if (value === 'Faible') {
+        return 'low-risk';
+    } else if (value === 'Moyen') {
+        return 'medium-risk';
+    } else if (value === 'Élevé') {
+        return 'high-risk';
+    } else {
+        return ''; // Classe par défaut si la valeur n'est pas reconnue
+    }
+}
+
+
