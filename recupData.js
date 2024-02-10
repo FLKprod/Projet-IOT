11b8ec4365e23aa6c32f45a3071eef21632d6e13
Jeremie <eyilink@oultook.com>
2024-02-09 21:01:11 -0500

    Ajout de la fonctionnalité de recherche par mots-clés --- Jeremie

--- recupData.js
+++ recupData.js
@@ -85,6 +85,81 @@ function loadTableData() {
     .catch(error => console.error('Erreur de chargement des données JSON:', error));  
 }
 
+
+function keywordSearch() {
+    var table = document.getElementById('vulnerabilityTable');
+    var keywordInput = document.getElementById('keywordInput')
+    var keywordSearch = keywordInput.value
+    const pubStartDate = "2021-08-04T00:00:00.000";
+    const pubEndDate = "2021-08-22T00:00:00.000";
+
+    fetch(`https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=${keywordSearch}&pubStartDate=${pubStartDate}&pubEndDate=${pubEndDate}`) 
+        .then(response => response.json())
+        .then(data => {
+            console.log("Réponse de l'API", data.vulnerabilities);
+            // Utilisez "data" comme nécessaire
+            data = data.vulnerabilities;
+            // Clear existing table
+            while (table.firstChild) {
+                table.removeChild(table.firstChild);
+            }
+            // Vérifier si data est un tableau et s'il contient au moins une ligne
+            if (Array.isArray(data) && data.length > 0) {
+                // 1ere ligne = en-têtes
+                var headerRow = table.createTHead().insertRow(0);
+                Object.keys(data[0].cve).forEach(header => {
+                    var cell = headerRow.insertCell();
+                    cell.appendChild(document.createTextNode(header));
+                });
+                headerRow.classList.add('header-row');
+
+                // Ajouter des lignes de data.json (à partir de la deuxième ligne)
+                data.forEach((rowData,rowIndex) => {
+                    var row = table.insertRow();
+                    Object.entries(rowData.cve).forEach(([key, value]) => {
+                        var cell = row.insertCell();
+                        // Si la valeur n'est pas un tableau, ajoutez-la normalement
+                        if (key == "descriptions"){
+                            if (localStorage.getItem('Language') == "English"){
+                                cell.appendChild(document.createTextNode(value[0].value));
+                                cell.firstChild.style.width = '200px';
+                            }
+                            else{
+                                cell.appendChild(document.createTextNode(value[1].value));
+                            }
+                            
+                        }
+                        else if (key == "metrics"){
+                            cell.appendChild(document.createTextNode(value.cvssMetricV2[0].source));
+                        }
+                        else if (key == "configurations"){
+                            cell.appendChild(document.createTextNode(value[0].nodes[0].cpeMatch[0].versionEndExcluding));
+                        }
+                        else if (key == "references"){
+                            textcase="";
+                            for (let i = 0; i < value.length; i++) {
+                                textcase += value[i].url;
+                                textcase += "\n"
+                            }
+                            cell.appendChild(document.createTextNode(textcase));
+                        }
+                        else{
+                            cell.appendChild(document.createTextNode(value));
+                        }
+                        // Ajouter une classe en fonction de la dernière colonne
+                        if (key == "vulnStatus") {
+                            cell.className = evaluateRisk(value);
+                        }
+                    });
+                });
+                sortTable();
+            } else {
+                console.error('Le fichier JSON est vide ou mal formaté.');
+            }
+    })
+    .catch(error => console.error('Erreur de chargement des données JSON:', error));  
+}
+
 function evaluateRisk(value) {
     // Ajouter des conditions pour déterminer la classe en fonction de la valeur
     if (value === 'Analyzed') {
