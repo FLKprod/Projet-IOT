beb03a4955e56e81574bed4b5f3faf413c322bb7
AB <ayoubbouasria@hotmail.fr>
2024-02-15 00:08:30 +0100

    reading json file

--- data.py
+++ data.py
@@ -1,19 +1,24 @@
 import json
 import requests
 
+with open('iot.json','r') as data:
+  file_contents = data.read()
+
+  temp = json.loads(file_contents)
+  print(type(temp),"\n")
+  for element in temp:
+    print('category ',element['category'])
+    #print(element['category'])
+    for element_brand in element['brands']:
+      print(element_brand)
+    
+
+   
+#   for category in file_contents:
+#     print(category)
+    
+#https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=Microsoft&pubStartDate=2021-07-04T19:15:08.000&pubEndDate=2021-08-05T00:00:00.000&resultsPerPage=20
 
 
 
-api_url = "https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=microsoft&pubStartDate=2021-08-04T19:15:08.000&pubEndDate=2021-08-05T00:00:00.000"
-
-response = requests.get(api_url)
-response.json()
-
-# response_display = json.dumps(response,indent=4)
-
-for element in response:
-    print(element)
-
-# print("tests")
-# print(response_display)
 
